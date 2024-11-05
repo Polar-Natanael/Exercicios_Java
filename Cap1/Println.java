90cb0e28b541cfbecaf20d5d2da28fe0e7e694ed
Natanael Santos <PolarCompilado@gmail.com>
2024-11-05 14:00:42 -0300

    Print

--- Cap1/Println.java
+++ Cap1/Println.java
@@ -1,11 +1,11 @@
-//programa de inpreção  de texto
+//programa de impreção de texto
 
 public class Println
 {
 	//Método main inicia a erxecução do aplicativo Java
 	public static void main (String[] args){
-		
-		System.out.println("Esse é um exercício para relembrar como dar um print");
+		System.out.print("Esse é o print ");
+		System.out.println("Esse é um exercício para relembrar como dar um println");
 	}
 	
 }
