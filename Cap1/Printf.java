7a0b5407c7b804d8eef986d6d589273ff36ded12
Natanael Santos <PolarCompilado@gmail.com>
2024-11-05 14:26:08 -0300

    Printf

---
+++ Cap1/Printf.java
@@ -0,0 +1,8 @@
+//Exibindo múltiplas linhas com o printf
+
+public class Printf
+{
+	public static void main(String[] args){
+	System.out.printf("%s%n%s%d%s","Essa linha está sendo imprimida com o uso do printf", "No dia:", 05, "  de novembro");
+	}
+}
