63ecfe6a212bd1e8212f1113293d81b4d2041c03
Natanael Santos <PolarCompilado@gmail.com>
2024-11-05 16:15:43 -0300

    IF

---
+++ Cap1/InstrucaoSe.java
@@ -0,0 +1,25 @@
+//Instrução if
+
+import java.util.Scanner;
+
+public class InstrucaoSe
+{
+	public static void main(String[] args){
+	 
+	 Scanner input = new Scanner(System.in);
+	 
+	 int number1;
+	 int number2;
+	 
+	 System.out.println("Forneça dois valores para que possamos comparalos");
+	 
+	 System.out.print("Primeiro valor: ");
+	 number1 = input.nextInt();
+	 System.out.print("Segundo valor: ");
+	 number2 = input.nextInt();
+	 
+	// if(number1 == number2)
+	 //System.out.printf;
+	 
+	}
+}
