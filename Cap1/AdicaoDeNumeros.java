6417a35f782fba11a46a8a161b00cdb907cecb86
Natanael Santos <PolarCompilado@gmail.com>
2024-11-05 15:07:34 -0300

    Soma de Valores inteiros

---
+++ Cap1/AdicaoDeNumeros.java
@@ -0,0 +1,30 @@
+//Programa onde são adicionados dois valores inteiros e então exibe sua soma.
+
+import java.util.Scanner;
+
+public class AdicaoDeNumeros
+{
+	public static void main(String[] args){
+	
+	//Cria uma Scanner
+	Scanner input = new Scanner(System.in);
+	
+	int number1;// valor 1
+	int number2;// valor 2
+	int sum;// soma dos valores
+	
+	System.out.println("Forneça dois valores inteiros para fazemos sua soma.");
+	
+	System.out.print("Primeiro Valor: ");
+	number1 = input.nextInt();//Lê o primeiro valor;
+	System.out.print("Segundo Valor: ");
+	number2 = input.nextInt();//Lé o segundo valor;
+	
+	sum = number1 + number2;// depois que temos o valor podemos fazer a soma e salvar na variavel.
+		
+	System.out.printf("%s%d%s%d%s%d", "A soma de ", number1," e ", number2, " é igual a: ", sum );
+
+	
+	}
+
+}
