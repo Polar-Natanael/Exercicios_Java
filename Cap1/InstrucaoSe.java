c34bfffa5d1ef735cc19239eaa4dd69b76189057
Natanael Santos <PolarCompilado@gmail.com>
2024-11-06 09:01:34 -0300

    Se

--- Cap1/InstrucaoSe.java
+++ Cap1/InstrucaoSe.java
@@ -18,8 +18,23 @@ public class InstrucaoSe
 	 System.out.print("Segundo valor: ");
 	 number2 = input.nextInt();
 	 
-	// if(number1 == number2)
-	 //System.out.printf;
+	 if(number1 == number2)//igual
+		System.out.printf("%d%s%d%n", number1, " = ", number2);
+	
+	 if(number1 != number2)// diferente
+		System.out.printf("%d%s%d%n", number1, " != ", number2);
+	 
+	 if(number1 > number2)//maior que
+		System.out.printf("%d%s%d%n", number1, " > ", number2);
+	 
+	 if(number1 < number2)//Menor que
+		System.out.printf("%d%s%d%n", number1, " < ", number2);
+	 
+	 if(number1 <= number2)// menor ou igual
+		System.out.printf("%n%s%n%d%s%d%n", "Nesse caso estamos analizando se for menor ou igual.", number1, "<=", number2);
+	
+	 if(number1 >= number2)// maior ou igual
+		System.out.printf("%n%s%n%d%s%d%n", "Nesse caso estamos analizando se for maior ou igual.", number1, ">=", number2);
 	 
 	}
 }
