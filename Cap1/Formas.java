90c9c3e5e7a13e84ca5575ce01985623d434633f
Natanael Santos <PolarCompilado@gmail.com>
2024-11-08 11:43:42 -0300

    formas

---
+++ Cap1/Formas.java
@@ -0,0 +1,17 @@
+// Criando formas com asteristicos 
+
+public class Formas
+{
+	public static void main (String[] args){
+		
+		System.out.println("* * * * * * * * *  \t   *** \t\t  * \t\t*");
+		System.out.println("*\t\t*  \t  *   *  \t ***   \t       * *");
+		System.out.println("*\t\t*  \t *     * \t*****  \t      *   *");
+		System.out.println("*\t\t*  \t *     * \t  *    \t     *     *");
+		System.out.println("*\t\t*  \t *     * \t  *    \t    *       *");
+		System.out.println("*\t\t*  \t *     * \t  *    \t     *     *");
+		System.out.println("*\t\t*  \t *     * \t  *    \t      *   *");
+		System.out.println("*\t\t*  \t  *   *  \t  *    \t       * *");
+		System.out.println("* * * * * * * * *  \t   *** \t\t  * \t\t*");
+	}
+}
