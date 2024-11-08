908001a98f34f1567cb1e8326b3c4ac132acd290
Natanael Santos <PolarCompilado@gmail.com>
2024-11-08 17:00:23 -0300

    Circulo

---
+++ Cap1/Circulo.java
@@ -0,0 +1,23 @@
+//Diametro, Circuferencia  e  círculo
+
+import java.util.Scanner;
+
+public class Circulo {
+	
+	public static void main(String[] args){
+		
+		Scanner input = new Scanner(System.in);
+		
+		int r;
+		
+		System.out.print("Forneça um valor inteiro para o raio do círculo:");
+		r = input.nextInt();
+		
+		System.out.printf("O diamentro do ciculo é %d", (2 * r));
+		System.out.println("");
+		System.out.printf("A circuferencia é %f", (2* 3.14159)*r);
+		System.out.println("");
+		System.out.printf("A area é %f", (3.14159 * r * r));
+		
+	}
+}
