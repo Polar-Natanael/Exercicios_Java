28b6391b18cfb6d40dd3feda270471461314dad1
Natanael Santos <PolarCompilado@gmail.com>
2024-11-11 16:39:33 -0300

    Create Account.java

---
+++ java_poo/Account.java
@@ -0,0 +1,17 @@
+//Classe Conta Que contém uma variável de instância name
+
+public class Account
+{
+	private String name;//variavel de instância
+	
+	//método para definir  o nome no objeto 
+	public void setName(String name)
+	{
+		this.name = name;//armazena o nome
+	}
+	//método para recuperar o nome do objeto
+	public String getName()
+	{
+		return name; //retorna valor do nome para o chamador 
+	}
+}
