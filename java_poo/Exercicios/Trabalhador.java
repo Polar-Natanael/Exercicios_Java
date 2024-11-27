1facab46a95186de4a9667944e08b7c2e0706df0
Natanael Santos <PolarCompilado@gmail.com>
2024-11-27 09:33:22 -0300

    Trabalhador

---
+++ java_poo/Exercicios/Trabalhador.java
@@ -0,0 +1,37 @@
+//Classe Trabalhador respossavel por guardar informações sobre o salario dos trabalhadores 
+
+public class Trabalhador
+{
+	private String name;
+	private String sobrenome;
+	private double salario;
+	
+	public Trabalhador(String name, String sobrenome, double salario)
+	{
+		this.name = name;
+		this.sobrenome = sobrenome;
+		
+		if(salario> 0.0)
+		this.salario = salario;
+	}	
+	
+	public void setName(String name){
+		this.name = name;
+	}
+	public String getName(){
+		return name;
+	}
+	
+	public void setSobrenome(String sobrenome){
+		this.sobrenome = sobrenome;
+	}
+	public String getSobrenome(){
+		return sobrenome;
+	}
+	
+	public void setSalario(double salario)
+	{
+		if(salario > 0.0)
+			this.salario = salario;
+	}
+}
