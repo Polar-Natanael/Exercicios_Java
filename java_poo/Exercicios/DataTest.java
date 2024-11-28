9c7eba2e61d68501d455be53c6cea4b3837f5654
Natanael Santos <PolarCompilado@gmail.com>
2024-11-28 10:32:34 -0300

    Update DataTest.java

--- java_poo/Exercicios/DataTest.java
+++ java_poo/Exercicios/DataTest.java
@@ -4,8 +4,33 @@ public class DataTest
 {
 	public static void main(String[] args)
 	{
+		int dia, mes, ano;
+		
+		
 		Data test1 = new Data(28, 11, 2024);
 		
 		test1.dataDisplay();
+		System.out.println();
+		
+		java.util.Scanner input = new Scanner(System.in);
+		
+		
+		
+		System.out.println("Olá vamos \"guarda\" esse momento por favor me informe:");
+		System.out.print("Qual é o dia do mes: ");
+		dia = input.nextInt();
+		
+		
+		System.out.print("Em que mes estamos(1-12):");
+		mes = input.nextInt();
+	
+		System.out.print("Em que ano estamos");
+		ano = input.nestInt();
+		System.out.println();
+		
+		Data test2 = new Data(dia, mes, ano),
+		test1.dataDisplay();
+		
+		
 	}
 }
